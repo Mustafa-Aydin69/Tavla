e004e2efe02b4089cde1b497330f11c8d1428144
MustafaAydin <mustafamulti12@gmail.com>
2026-04-30 13:19:35 +0300

    UPDATE : game.py
    server'da state = session.game.get_state() satırı var ama game.py dosyasına baktığım zaman game_state fonksiyonu yazılmamış.  Bu yüzden game.py dosyasına game_state fonksiyonunu ekledim

--- shared/protocol.py
+++ shared/protocol.py
@@ -1,24 +1,22 @@
 import json
 
-def encode(msg:dict)->bytes:
+
+def encode(msg: dict) -> bytes:
     return (json.dumps(msg) + "\n").encode("utf-8")
+
 
 def decode(line: str) -> dict:
     return json.loads(line)
 
 
-#mesaj tipleri
+# mesaj tipleri
 
 JOIN = "JOIN"
 WAITING = "WAITING"
 MATCH = "MATCH"
-ROLL="ROLL"
+ROLL = "ROLL"
 MOVE = "MOVE"
 STATE = "STATE"
 REJECT = "REJECT"
 TURN = "TURN"
 GAME_OVER = "GAME_OVER"
-
-
-
-    
