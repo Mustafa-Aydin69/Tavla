8e56129c025d43aa3b5177a96a2e3e86432cb212
MustafaAydin <mustafamulti12@gmail.com>
2026-04-30 17:09:03 +0300

    UPDATE: client.py
    Kullanıcı komut girmesi ve bunun server'a iletilmesi

--- client/client.py
+++ client/client.py
@@ -1,5 +1,8 @@
 import socket
 
+
+from shared.protocol import encode, decode
+
 HOST = "127.0.0.1"
 PORT = 5000
 
@@ -9,11 +12,26 @@ def start_client():
     sock.connect((HOST, PORT))
     print("Connected to server")
     while True:
+        cmd = input(">>> ")
+
+        if cmd == "roll":
+            sock.sendall(encode({"type": "ROLL"}))
+
+        elif cmd.startswith("move"):
+            try:
+                _, start, die = cmd.split()
+                sock.sendall(
+                    encode({"type": "MOVE", "moves": [(int(start), int(die))]})
+                )
+            except ValueError:
+                print("Usage: move <start> <die>")
+
         data = sock.recv(1024)
         if not data:
-            print("Server stopped")
+            print("Disconnected.")
             break
-        print(f"Server: {data.decode()}")
+
+        print("SERVER:", data.decode())
 
 
 if __name__ == "__main__":
